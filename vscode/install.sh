10ba706d53a194fd53d3d393da54201744c0fc78
kmctown <kciccarello@gmail.com>
2019-03-10 20:31:07 -0400

    Move vscode install to bash

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,24 +1,30 @@
-#!/bin/zsh
+#!/bin/bash
 
-echo "Installing vscode..."
+if test $(which brew)
+then
+  echo "Installing vscode..."
 
-brew cask install visual-studio-code
+  brew cask install visual-studio-code
 
-echo "Installing vscode extensions..."
+  echo "Installing vscode extensions..."
 
-# Run `code --list-extensions` to check current extensions
-CODE_EXTENSIONS=(
-  TsumiNa.Seti-theme
-  donjayamanne.githistory
-  esbenp.prettier-vscode
-  GitHub.vscode-pull-request-github
-  jpoissonnier.vscode-styled-components
-  ms-python.python
-  ms-vscode.atom-keybindings
-  qinjia.seti-icons
-  sensourceinc.vscode-sql-beautify
-  vscodevim.vim
-)
+  # Run `code --list-extensions` to check current extensions
+  declare -a CODE_EXTENSIONS=(
+    TsumiNa.Seti-theme
+    donjayamanne.githistory
+    esbenp.prettier-vscode
+    GitHub.vscode-pull-request-github
+    jpoissonnier.vscode-styled-components
+    ms-python.python
+    ms-vscode.atom-keybindings
+    qinjia.seti-icons
+    sensourceinc.vscode-sql-beautify
+    vscodevim.vim
+  )
 
-for ext in $CODE_EXTENSIONS ; code --install-extension $ext
+  for ext in ${CODE_EXTENSIONS[@]}; do
+     code --install-extension $ext
+  done
+fi
 
+exit 0
